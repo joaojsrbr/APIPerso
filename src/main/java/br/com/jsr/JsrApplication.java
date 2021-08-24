ab58cbbb573fa18d0ce769f8291145f85d6680bc
joaojsrbr <joaovitor.jsr@gmail.com>
2021-08-24 13:48:49 -0300

    new

--- src/main/java/br/com/jsr/JsrApplication.java
+++ src/main/java/br/com/jsr/JsrApplication.java
@@ -2,6 +2,7 @@ package br.com.jsr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JsrApplication {
---
+++ src/main/java/br/com/jsr/controller/PersonController.java
@@ -0,0 +1,13 @@
+package br.com.jsr.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+
+
+}
