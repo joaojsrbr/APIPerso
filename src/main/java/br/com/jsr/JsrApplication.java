84e17f9b9921f62ddd42b6bb22c2c8c202f4f9f6
joaojsrbr <joaovitor.jsr@gmail.com>
2021-08-24 12:37:49 -0300

    Initial commit

---
+++ src/main/java/br/com/jsr/JsrApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jsr/JsrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
