ebf0439f1a931ff530890405ab5ae938dd9af7a5
joaojsrbr <joaovitor.jsr@gmail.com>
2021-08-25 02:06:27 -0300

    new

--- src/main/java/br/com/jsr/exception/PersonNotFoundException.java
+++ src/main/java/br/com/jsr/exception/PersonNotFoundException.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class PersonNotFoundException extends Exception {
 
     public PersonNotFoundException(Long id) {
-        super("Person not found with ID " + id);
+        super("Pessoa não encontrada com ID" + id);
     }
 }
