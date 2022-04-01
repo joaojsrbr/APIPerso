46c33f96c52da8c3dbbc609c9f028f6970035134
João Vitor <joaovitor.jsr@gmail.com>
2022-04-01 19:40:49 -0300

    make it better

--- src/main/java/br/com/jsr/mapper/PersonMapper.java
+++ src/main/java/br/com/jsr/mapper/PersonMapper.java
@@ -11,8 +11,7 @@ import org.mapstruct.factory.Mappers;
 public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
-
-    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd/MM/yyyy")
+    
     Person toModel(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
