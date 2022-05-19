5ceda5269b6367cd7c90702e45648effc11c5b5b
João Vitor <joaovitor.jsr@gmail.com>
2022-05-19 10:56:04 -0300

    new

--- src/main/java/br/com/jsr/entity/Person.java
+++ src/main/java/br/com/jsr/entity/Person.java
@@ -12,11 +12,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
+//import javax.validation.constraints.NotNull;
+//import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
+//import java.time.LocalTime;
+//import java.util.Date;
 
 @Entity
 @Data
