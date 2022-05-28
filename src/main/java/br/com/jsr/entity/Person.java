88fbf3efb7b20d808fb861f18fdb5048248decbf
João Vitor <joaovitor.jsr@gmail.com>
2022-05-27 23:49:01 -0300

    new

--- src/main/java/br/com/jsr/entity/Person.java
+++ src/main/java/br/com/jsr/entity/Person.java
@@ -2,39 +2,36 @@ package br.com.jsr.entity;
 
 
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.sql.Date;
+import java.util.Objects;
+
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
         @Id
-        @NotBlank
         @ApiModelProperty(value = "ID")
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
         @ApiModelProperty(value = "Primeiro Nome")
-        @NotBlank
         @Column(nullable = false)
         private String firstName;
 
         @ApiModelProperty(value = "Último Nome")
-        @NotBlank
         @Column(nullable = false)
         private String lastName;
 
@@ -42,17 +39,32 @@ public class Person {
 //        @Column(nullable = false, unique = true)
 //        private String cpf;
 
-        @Column(name = "created_at")
         @CreationTimestamp
-        private LocalDateTime createdAt;
+        @Temporal(TemporalType.TIMESTAMP)
+        @Column(name = "create_date")
+        private Date createDate;
 
-        @Column(name = "updated_at")
         @UpdateTimestamp
-        private LocalDateTime updatedAt;
+        @Temporal(TemporalType.TIMESTAMP)
+        @Column(name = "modify_date")
+        private Date modifyDate;
 
 //        @ApiModelProperty(value = "Telefones")
 //        @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.REMOVE,CascadeType.MERGE})
 //        private List<Phone> phones;
-    }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+                Person person = (Person) o;
+                return id != null && Objects.equals(id, person.id);
+        }
+
+        @Override
+        public int hashCode() {
+                return getClass().hashCode();
+        }
+}
 
 
